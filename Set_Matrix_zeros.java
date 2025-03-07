3c2d79df936731cae1896bfc2ed1eed6fa46fd0a
kbhujbal <kbhujbal@usc.edu>
2025-03-06 22:09:49 -0800

    Set Matrix Zeros

--- Set_Matrix_zeros.java
+++ Set_Matrix_zeros.java
@@ -5,7 +5,6 @@ public class Set_Matrix_zeros {
         boolean firstRowZero = false;
         boolean firstColZero = false;
 
-        // Check if the first row should be zeroed
         for (int j = 0; j < matrix[0].length; j++) {
             if (matrix[0][j] == 0) {
                 firstRowZero = true;
@@ -13,7 +12,6 @@ public class Set_Matrix_zeros {
             }
         }
 
-        // Check if the first column should be zeroed
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
                 firstColZero = true;
@@ -21,7 +19,6 @@ public class Set_Matrix_zeros {
             }
         }
 
-        // Use the first row and first column to mark zeros
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
@@ -31,7 +28,6 @@ public class Set_Matrix_zeros {
             }
         }
 
-        // Zero out cells based on marks in the first row and first column
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -40,14 +36,12 @@ public class Set_Matrix_zeros {
             }
         }
 
-        // Zero out the first row if needed
         if (firstRowZero) {
             for (int j = 0; j < matrix[0].length; j++) {
                 matrix[0][j] = 0;
             }
         }
 
-        // Zero out the first column if needed
         if (firstColZero) {
             for (int i = 0; i < matrix.length; i++) {
                 matrix[i][0] = 0;
@@ -64,7 +58,6 @@ public class Set_Matrix_zeros {
         };
         solution.setZeroes(matrix);
 
-        // Print the modified matrix
         for (int[] row : matrix) {
             for (int num : row) {
                 System.out.print(num + " ");
