8bc660ac6855f4a2a1455b9ac7210df981bd3345
kbhujbal <kbhujbal@usc.edu>
2025-04-05 11:28:25 -0700

    Sum of all subset XOR totals

---
+++ SubsetXORSum.java
@@ -0,0 +1,29 @@
+public class SubsetXORSum {
+
+    // Solution method using DFS
+    public int subsetXORSum(int[] nums) {
+        return dfs(nums, 0, 0);
+    }
+
+    private int dfs(int[] nums, int index, int currentXor) {
+        if (index == nums.length) {
+            return currentXor;
+        }
+        // Include the current element
+        int withCurrent = dfs(nums, index + 1, currentXor ^ nums[index]);
+        // Exclude the current element
+        int withoutCurrent = dfs(nums, index + 1, currentXor);
+        return withCurrent + withoutCurrent;
+    }
+
+    // Main method to run test cases
+    public static void main(String[] args) {
+        SubsetXORSum solution = new SubsetXORSum();
+
+        int[] nums = {5, 1, 6};
+        int result = solution.subsetXORSum(nums);
+
+        System.out.println("Input: nums = [5, 1, 6]");
+        System.out.println("Output: " + result); // Expected output: 28
+    }
+}
