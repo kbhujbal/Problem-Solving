4896ccf1a419f2f2b3f011f49d83c3275ae2e62e
kbhujbal <kbhujbal@usc.edu>
2025-04-05 23:03:13 -0700

    Search Element in Rotated Sorted Array 1

---
+++ Binary Search/Search_element_in_rotated_sorted_array_1.java
@@ -0,0 +1,37 @@
+public class Search_element_in_rotated_sorted_array_1{
+    public static void main(String[] args) {
+        int [] arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+        int x = 1;
+
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) { 
+            int mid = (low + high)/2;
+
+            if (arr[mid] == x) {
+                System.out.println("Element found at index: " + mid);
+                return;
+            }
+
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= x && x <= arr[mid]) {
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (arr[mid] <= x && x <= arr[high]) {
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                    
+                }
+            }
+        }
+        System.out.println("Element not found");
+    }
+}
