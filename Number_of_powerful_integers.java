e383fe4c3d7905d383849a011aad8d1dfb09b991
kbhujbal <kbhujbal@usc.edu>
2025-04-09 23:58:30 -0700

    Count the Number of Powerful Integers

--- Number_of_powerful_integers.java
+++ Number_of_powerful_integers.java
@@ -1,40 +1,58 @@
 public class Number_of_powerful_integers {
-    private String s;
+
+    public static void main(String[] args) {
+        long start = 1;
+        long finish = 6000;
+        int limit = 4;
+        String s = "124";
+
+        Solution sol = new Solution();
+        long result = sol.numberOfPowerfulInt(start, finish, limit, s);
+        System.out.println("Output: " + result);  // Expected: 5
+    }
+}
+
+class Solution {
+    private String suffix;
     private int limit;
-    private String numStr;
-    private Long[][][] memo;
-
+    
     public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
-        this.s = s;
+        this.suffix = s;
         this.limit = limit;
-        return countPowerfulIntegers(finish) - countPowerfulIntegers(start - 1);
+        return countUpTo(finish) - countUpTo(start - 1);
     }
 
-    private long countPowerfulIntegers(long upTo) {
-        this.numStr = String.valueOf(upTo);
-        int n = numStr.length();
-        this.memo = new Long[n][2][2];
-        return dfs(0, true, false);
+    private long countUpTo(long num) {
+        String strNum = String.valueOf(num);
+        return dfs(strNum, 0, true, "", new Long[strNum.length()][2][]);
     }
 
-    private long dfs(int pos, boolean isTight, boolean hasSuffix) {
-        if (pos == numStr.length()) {
-            return hasSuffix ? 1 : 0;
-        }
-        if (memo[pos][isTight ? 1 : 0][hasSuffix ? 1 : 0] != null) {
-            return memo[pos][isTight ? 1 : 0][hasSuffix ? 1 : 0];
-        }
-
-        int upperBound = isTight ? numStr.charAt(pos) - '0' : limit;
-        long count = 0;
-
-        for (int digit = 0; digit <= upperBound; digit++) {
-            if (digit > limit) continue;
-            boolean newHasSuffix = hasSuffix || (digit == s.charAt(0) - '0' && pos + s.length() <= numStr.length() && numStr.startsWith(s, pos));
-            count += dfs(pos + 1, isTight && (digit == upperBound), newHasSuffix);
+    private long dfs(String num, int pos, boolean tight, String curr, Long[][][] dp) {
+        if (pos == num.length()) {
+            if (curr.length() < suffix.length()) return 0;
+            if (curr.endsWith(suffix)) return 1;
+            return 0;
         }
 
-        memo[pos][isTight ? 1 : 0][hasSuffix ? 1 : 0] = count;
-        return count;
+        int t = tight ? 1 : 0;
+        if (dp[pos][t] == null) dp[pos][t] = new Long[10000];  // 10^4 digits just to be safe (curr string cache)
+        int currHash = curr.hashCode() & 0x7FFFFFFF; // positive hash as index
+        currHash %= 10000;
+
+        if (dp[pos][t][currHash] != null) return dp[pos][t][currHash];
+
+        long res = 0;
+        int maxDigit = tight ? num.charAt(pos) - '0' : 9;
+
+        for (int d = 0; d <= maxDigit; d++) {
+            if (d > limit) continue;
+
+            String next = (curr.equals("0") ? "" : curr) + d;
+            boolean nextTight = tight && (d == maxDigit);
+            res += dfs(num, pos + 1, nextTight, next, dp);
+        }
+
+        dp[pos][t][currHash] = res;
+        return res;
     }
 }
