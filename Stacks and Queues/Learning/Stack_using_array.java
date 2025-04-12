0882cbf2d26f10dc178db7d4f4b1adc2f3dc6e3c
kbhujbal <kbhujbal@usc.edu>
2025-04-11 23:15:32 -0700

    Stack using array

--- Stacks and Queues/Learning/Stack_using_array.java
+++ Stacks and Queues/Learning/Stack_using_array.java
@@ -1,5 +1,35 @@
-public class Stack_using_array{
+
+public class Stack_using_array {
     public static void main(String[] args) {
-        
+
+        stack s = new stack();
+        s.push(6);
+        s.push(3);
+        s.push(7);
+        System.out.println("Top of the stack before deleting any element " + s.top());
+        System.out.println("Size of the stack before deleting any element " + s.size());
+        System.out.println("The element deleted is " + s.pop());
+        System.out.println("Size of the stack after deleting an element " + s.size());
+        System.out.println("Top of the stack after deleting an element " + s.top());
+    }
+}
+class stack {
+    int size = 10000;
+    int arr[] = new int[size];
+    int top = -1;
+    void push(int x) {
+        top++;
+        arr[top] = x;
+    }
+    int pop() {
+        int x = arr[top];
+        top--;
+        return x;
+    }
+    int top() {
+        return arr[top];
+    }
+    int size() {
+        return top + 1;
     }
 }
