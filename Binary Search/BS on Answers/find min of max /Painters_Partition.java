66b527eff14040f1aa885d785fc07e178c7746af
kbhujbal <kbhujbal@usc.edu>
2025-04-13 16:55:42 -0700

    Split Array Largest Sum

---
+++ Binary Search/BS on Answers/find min of max /Painters_Partition.java
@@ -0,0 +1,5 @@
+public class Painters_Partition{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Binary Search/BS on Answers/find min of max /Split_array_largest_sum.java
@@ -0,0 +1,5 @@
+public class Split_array_largest_sum{
+    public static void main(String[] args) {
+        
+    }
+}
