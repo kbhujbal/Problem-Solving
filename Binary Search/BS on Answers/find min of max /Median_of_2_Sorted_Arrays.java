3c7cc730164497061664191398476eeb4af8f9c8
kbhujbal <kbhujbal@usc.edu>
2025-04-15 18:44:05 -0700

    Median of 2 Sorted Arrays

---
+++ Binary Search/BS on Answers/find min of max /Median_of_2_Sorted_Arrays.java
@@ -0,0 +1,8 @@
+public class Median_of_2_Sorted_Arrays{
+    public static void main(String[] args) {
+        int[] nums1 = {1, 4, 7, 10, 12};
+        int[] nums2 = {2, 3, 6, 15};
+
+        
+    }
+}
