fc93abf3afa01fa8f13a8b04650e6c0923a9d9e4
kbhujbal <kbhujbal@usc.edu>
2025-04-17 01:55:40 -0700

    Peak Element in 2D Matrix

---
+++ Binary Search/BS on 2D Arrays/Peak_Element_2D_Matrix.java
@@ -0,0 +1,51 @@
+public class Peak_Element_2D_Matrix{
+    public static void main(String[] args) {
+        boolean found = false;
+        int[][] matrix = {
+            {1, 4},
+            {3, 2}
+        };
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int low = 0, high = m - 1;
+
+        while (low <= high) { 
+            int mid = (low + high)/2;
+
+            int rowIdx = maxElementIndex(matrix, n, mid);
+
+            int left = (mid - 1 >= 0) ? matrix[rowIdx][mid - 1] : -1;
+            int right = (mid + 1 < m) ? matrix[rowIdx][mid + 1] : -1;
+
+            if (matrix[rowIdx][mid] > left && matrix[rowIdx][mid] > right) {
+                System.out.println("Peak Element found at: " + rowIdx + ", " + mid);
+                found = true;
+                break;
+            }
+            else if (matrix[rowIdx][mid] < left) {
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+
+    private static int maxElementIndex(int[][] matrix, int n, int mid){
+        int maxValue = 0;
+        int index = 0;
+
+        for(int i = 0; i < n; i++){
+            if(matrix[i][mid] > maxValue){
+                maxValue = matrix[i][mid];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
