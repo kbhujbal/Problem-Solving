bc7a01eef5ef93c557d153f85f987eb41b2b5196
kbhujbal <kbhujbal@usc.edu>
2025-04-28 23:48:20 -0700

    Practice

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -1,21 +1,5 @@
 public class Practice{
     public static void main(String[] args) {
-        int[] arr = {1, 1, 5};
-
-        int i = arr.length - 2;
-        while (i >= 0 && arr[i] >= arr[i + 1]) { 
-            i--;
-        }
-
-        int j = arr.length - 1;
-        while (j >= 0 && arr[j] <= arr[i]) { 
-            j--;
-        }
-
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-
         
     }
 }
