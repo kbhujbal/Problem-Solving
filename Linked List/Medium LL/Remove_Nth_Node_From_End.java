e8d1bbef886dceec0748877e91d71cce66378da8
kbhujbal <kbhujbal@usc.edu>
2025-05-02 00:08:39 -0700

    Remove Nth Node from End of Linked List

---
+++ Linked List/Medium LL/Remove_Nth_Node_From_End.java
@@ -0,0 +1,13 @@
+public class Remove_Nth_Node_From_End{
+    public static void main(String[] args) {
+        Linked_List list = new Linked_List();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+
+        
+    }
+}
