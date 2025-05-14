31796c7ed4ee17dd1827a376ea4cdc15fc9ccd36
kbhujbal <kbhujbal@usc.edu>
2025-05-14 13:03:45 -0700

    Binary Tree

---
+++ Binary Tree/Traversal/Node.java
@@ -0,0 +1,11 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ Binary Tree/Traversal/Preorder_Traversal.java
@@ -0,0 +1,5 @@
+public class Preorder_Traversal{
+    public static void main(String[] args) {
+        
+    }
+}
