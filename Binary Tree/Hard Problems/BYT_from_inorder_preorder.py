17f86ac2732ee55ade77438882f0bb5cb760b2e7
kbhujbal <kbhujbal@usc.edu>
2025-06-25 00:56:48 -0700

    Binary Tree Conversions

---
+++ Binary Tree/Hard Problems/BYT_from_inorder_preorder.py
@@ -0,0 +1,14 @@
+from Node import Node
+
+
+def build_tree(inorder, preorder):
+
+    return root
+
+
+if __name__ == "__main__":
+    inorder = [40, 20, 50, 10, 60, 30]
+    preorder = [10, 20, 40, 50, 30, 60]
+
+    ans = build_tree(inorder, preorder)
+    print(ans)
