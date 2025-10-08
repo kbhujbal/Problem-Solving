110706f7cd2767fcd4f64c74c5ddeac374a250b8
kbhujbal <kbhujbal@usc.edu>
2025-10-07 22:48:48 -0700

    Successful Pairs

--- Daily Leetcode Challenge/Successful_Pairs.py
+++ Daily Leetcode Challenge/Successful_Pairs.py
@@ -9,6 +9,9 @@ def successfulPairs(spells, potions, success):
         for spell in spells:
             min_potion_strength = (success + spell - 1) // spell
             
+            # Use binary search to find the index of the first potion that
+            # is strong enough. bisect_left finds the insertion point, which
+            # corresponds to the first element >= min_potion_strength.
             first_successful_idx = bisect.bisect_left(potions, min_potion_strength)
             
             count = m - first_successful_idx
