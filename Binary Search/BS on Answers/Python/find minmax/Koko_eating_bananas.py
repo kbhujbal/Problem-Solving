6c596d3adbadfb2f862441ce48f9fe1fe9a35c43
kbhujbal <kbhujbal@usc.edu>
2025-10-09 11:59:00 -0700

    BS python

---
+++ Binary Search/BS on Answers/Python/find minmax/Koko_eating_bananas.py
@@ -0,0 +1,28 @@
+import math
+
+
+def koko_eating_banana(piles, h):
+    low, high = 1, len(piles)
+
+    res = high
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        total_hours = 0
+
+        for p in piles:
+            total_hours += math.ceil(p / mid)
+        
+        if total_hours <= h:
+            res = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return res
+
+
+
+piles = [30,11,23,4,20]
+h = 5
