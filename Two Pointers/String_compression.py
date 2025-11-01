68028656faa533de56be76c35fdfb67561cf40d9
kbhujbal <kbhujbal@usc.edu>
2025-10-31 22:05:06 -0700

    String compression

--- Two Pointers/String_compression.py
+++ Two Pointers/String_compression.py
@@ -5,28 +5,26 @@
 def string_compression(chars):
     n = len(chars)
 
-    if n < 2:
-        return n
+    read_ptr = 0
+    write_ptr = 0
 
-    write_idx = 0
-    read_idx = 0
-
-    while read_idx < n:
-        current_char = chars[read_idx]
+    while read_ptr < n:
+        current_char = chars[read_ptr]
         count = 0
 
-        while read_idx < n and chars[read_idx] == current_char:
-            read_idx += 1
+        while read_ptr < n and chars[read_ptr] == current_char:
+            read_ptr += 1
             count += 1
-
-        chars[write_idx] = current_char
-        write_idx += 1
+        
+        chars[write_ptr] = current_char
+        write_ptr += 1
 
         if count > 1:
             for digit in str(count):
-                chars[write_idx] = digit
-                write_idx += 1
+                chars[write_ptr] = digit
+                write_ptr += 1
         
+    
 
 chars = ["a","a","b","b","c","c","c"]
 string_compression(chars)
