f71ef965545d38322daf3302e4303b4ff8b10c8e
kbhujbal <kbhujbal@usc.edu>
2025-11-08 14:46:16 -0800

    Permutation stirng

--- Sliding Window/Permutation_string.py
+++ Sliding Window/Permutation_string.py
@@ -17,8 +17,17 @@ def permutation_string(s1, s2):
     if s1_counts == window_counts:
         return True
     
+    for i in range(len1, len2):
+        window_counts[char_index(s2[i])] += 1
+        window_counts[char_index(s2[i - len1])] -= 1
+    
+        if s1_counts == window_counts:
+            return True
+
+    return False
     
 
 
 s1 = "ab"
 s2 = "eidbaooo"
+print(permutation_string(s1, s2))
