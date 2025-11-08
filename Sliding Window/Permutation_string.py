a956bc2caffa46cca2aaa1e0ca9bbab8b9066abe
kbhujbal <kbhujbal@usc.edu>
2025-11-07 21:12:08 -0800

    Permutation string

---
+++ Sliding Window/Permutation_string.py
@@ -0,0 +1,24 @@
+def permutation_string(s1, s2):
+    len1, len2 = len(s1), len(s2)
+
+    if len1 > len2:
+        return False
+    
+    s1_counts = [0] * 26
+    window_counts = [0] * 26
+
+    def char_index(char):
+        return ord(char) - ord('a')
+    
+    for i in range(len1):
+        s1_counts[char_index(s1[i])] += 1
+        window_counts[char_index(s2[i])] += 1
+    
+    if s1_counts == window_counts:
+        return True
+    
+    
+
+
+s1 = "ab"
+s2 = "eidbaooo"
